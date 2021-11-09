4bfa6ed9609f8946e1931000c4e93b479f62eec9
Max05120 <93513430+Max05120@users.noreply.github.com>
2021-11-09 12:08:04 +0530

    updated

--- machinelearningbasic/settings.py
+++ machinelearningbasic/settings.py
@@ -121,12 +121,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
+STATIC_URL = '/basics/static/'
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'basics/static'))
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 MEDIA_URL = '/images/'
 
